3d8574d742c5819d3232e5b8bd9b34f7519f6510
Cate Uselton <cathode.use@gmail.com>
2014-10-07 16:41:11 -0700

    added blank signup page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
 
   get "/posts", to: "posts#index"
+
+  root "posts#index"
+
   
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
